ca673a7db9b3f05d460ca8164410a46edd039a25
Sophia Don Tranho <sophiadnth@gmail.com>
2024-12-10 03:44:01 -0800

    adding typewriter

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,8 +3,27 @@ import { FaLocationArrow } from "react-icons/fa6";
 import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import { TypewriterEffect } from "./ui/TypeWriterEffect";
 
 const Hero = () => {
+  const words = [
+    {
+      text: "software developer ",
+    },
+    {
+      text: "animator ",
+    },
+    {
+      text: "student ",
+    },
+    {
+      text: "designer ",
+    },
+    {
+      text: "Sophia!",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
   return (
     <div className="pb-20 pt-36">
       <div>
@@ -38,21 +57,20 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <p className="uppercase tracking-widest text-xs text-center text-black-100 max-w-80">
-            Made by Sophia Don Tranho
-          </p>
 
           {/**
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="bleh bleh bleh bleh bleh bleh"
+            words="Hi, I'm Sophia!"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2l">
+          <TypewriterEffect words={words} />
+
+          {/* <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2l">
             Hi! I&apos;m Sophia, a CS student.
-          </p>
+          </p> */}
 
           <a href="#about">
             <MagicButton
