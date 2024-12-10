32b1beda27e0a41856b8ed373839a728e1d98d62
Sophia Don Tranho <sophiadnth@gmail.com>
2024-12-09 20:23:03 -0800

    15:40 of tutorial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,11 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+        black: {
+          DEFAULT: "#000",
+          100: '#000319'
+        }
+      }
     },
   },
   plugins: [],
