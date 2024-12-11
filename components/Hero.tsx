11360c23c231a0b93ac470792298ea1a05c186e6
Sophia Don Tranho <sophiadnth@gmail.com>
2024-12-10 17:04:07 -0800

    asdjlasd

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,7 +29,7 @@ const Hero = () => {
       </div>
 
       {/* Background grid */}
-      <div
+      {/* <div
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
         absolute top-0 left-0 flex items-center justify-center"
       >
@@ -37,7 +37,7 @@ const Hero = () => {
           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
-      </div>
+      </div> */}
 
       {/* Main content */}
       <div className="flex justify-center relative my-20 z-10">
