0d6575e0e974fe26222e99331c26376369d469f4
Sophia Don Tranho <sophiadnth@gmail.com>
2024-12-10 21:33:27 -0800

    is this working idk

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-white-100 flex justify-left items-left flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-white flex justify-left items-left flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={[
           {name: "Home", link: "/", icon: <FaHome />}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,9 @@ const Hero = () => {
   ];
 
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-20 pt-36 bg-white-akari-white">
+      
+      
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
