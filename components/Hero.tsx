395589335cdd64f2f4dddc170fd3a5bfe221dd95
Sophia Don Tranho <sophiadnth@gmail.com>
2024-12-11 01:51:45 -0800

    background gradient

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,5 @@
-import { FaLocationArrow } from "react-icons/fa6";
-import MagicButton from "./MagicButton";
-import { Spotlight } from "./ui/Spotlight";
-import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import { TypeWriterEffect } from "./ui/TypeWriterEffect";
+import { BackgroundGradientAnimation } from "./ui/BackgroundGradientAnimation";
 import { Rain } from "./ui/Rain";
 
 const Hero = () => {
@@ -17,44 +14,34 @@ const Hero = () => {
 
   return (
     <div className="bg-white-akari-white">
-      <Rain>
-        <div>
-          <Spotlight
-            className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-            fill="white"
-          />
-          <Spotlight
-            className="h-[80vh] w-[50vw] top-10 left-full"
-            fill="purple"
-          />
-          <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
-        </div>
+      <BackgroundGradientAnimation>
+        <Rain>
+          {/* Main content */}
+          <div className="flex justify-center relative my-30 z-10">
+            <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col">
+              <div className="flex flex-col md:flex-row items-center md:items-start gap-20">
+                {/* Text Section with adjusted margin-top */}
+                <div className="flex-2 md:mr-20 lg:mr-30 mt-20 md:mt-30 lg:mt-40">
+                  <h2 className="text-left text-[40px] md:text-4xl lg:text-5xl">
+                    Hi, welcome to my portfolio!
+                  </h2>
+                  <TypeWriterEffect
+                    prefix="I'm "
+                    words={words}
+                    className="text-left sm:text-base md:text-xl lg:text-2xl xl:text-4xl"
+                    cursorClassName="bg-blue-400"
+                  />
+                </div>
 
-        {/* Main content */}
-        <div className="flex justify-center relative my-30 z-10">
-          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col">
-            <div className="flex flex-col md:flex-row items-center md:items-start gap-20">
-              {/* Text Section with adjusted margin-top */}
-              <div className="flex-2 md:mr-20 lg:mr-30 mt-20 md:mt-30 lg:mt-40">
-                <h2 className="text-left text-[40px] md:text-4xl lg:text-5xl">
-                  Hi, welcome to my portfolio!
-                </h2>
-                <TypeWriterEffect
-                  prefix="I'm "
-                  words={words}
-                  className="text-left sm:text-base md:text-xl lg:text-2xl xl:text-4xl"
-                  cursorClassName="bg-blue-400"
-                />
-              </div>
-
-              {/* Image Section remains unaffected */}
-              <div className="flex-1 flex justify-center md:justify-end">
-                <img src="/kitty.png" alt="Kitty" className="w-48 h-auto rounded-lg" />
+                {/* Image Section remains unaffected */}
+                <div className="flex-1 flex justify-center md:justify-end">
+                  <img src="/kitty.png" alt="Kitty" className="w-48 h-auto rounded-lg" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Rain>
+        </Rain>
+      </BackgroundGradientAnimation>
     </div>
   );
 };
