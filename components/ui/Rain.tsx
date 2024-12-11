5d3dc1e9dbc04bf1c000605efbd2174901e06b91
Sophia Don Tranho <sophiadnth@gmail.com>
2024-12-11 02:18:12 -0800

    rain looks like ash lol

--- components/ui/Rain.tsx
+++ components/ui/Rain.tsx
@@ -88,7 +88,7 @@ export const Rain = ({
             {children}
             <div
                 ref={containerRef}
-                className="absolute bottom-0 bg-neutral-100 w-full inset-x-0 pointer-events-none"
+                className="absolute bottom-0 bg-white-akari-white w-full inset-x-0 pointer-events-none"
                 style={{
                     boxShadow:
                         "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset",
@@ -242,7 +242,7 @@ const Explosion = ({ ...props }: React.HTMLProps<HTMLDivElement>) => {
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
                 transition={{ duration: 1.5, ease: "easeOut" }}
-                className="absolute -inset-x-10 top-0 m-auto h-2 w-10 rounded-full bg-gradient-to-r from-[rgba(229,231,231,0.9)] to-[rgba(229,231,231,0.6)] blur-sm"
+                className="absolute -inset-x-10 top-0 m-auto h-2 w-10 rounded-full bg-gradient-to-r from-[rgba(147,180,238,0.9)] to-[rgba(109,119,127,0.6)] blur-sm"
             ></motion.div>
             {spans.map((span) => (
                 <motion.span
@@ -254,7 +254,7 @@ const Explosion = ({ ...props }: React.HTMLProps<HTMLDivElement>) => {
                         opacity: 0,
                     }}
                     transition={{ duration: Math.random() * 1.5 + 0.5, ease: "easeOut" }}
-                    className="absolute h-1 w-1 rounded-full bg-gradient-to-b from-[rgba(229,231,231,0.9)] to-[rgba(229,231,231,0.6)] blur-sm"
+                    className="absolute h-1 w-1 rounded-full bg-gradient-to-b from-[rgba(174,201,229,0.9)] to-[rgba(67,69,69,0.6)] blur-sm"
                 /> 
             ))}
         </div>
