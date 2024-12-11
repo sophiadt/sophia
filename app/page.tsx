4bdda699f4bf49a0f79273beb2030706705b2aa0
Sophia Don Tranho <sophiadnth@gmail.com>
2024-12-11 01:15:58 -0800

    rain and center

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-white flex justify-left items-left flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+    <main className="relative bg-white flex justify-center items-center flex-col overflow-hidden mx-auto">
+      <div className="w-full">
         <FloatingNav navItems={[
           {name: "Home", link: "/", icon: <FaHome />}
         ]} />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import { TypeWriterEffect } from "./ui/TypeWriterEffect";
+import { Rain } from "./ui/Rain";
 
 const Hero = () => {
   const words = [
@@ -15,59 +16,60 @@ const Hero = () => {
   ];
 
   return (
-    <div className="pb-20 pt-36 bg-white-akari-white">
-      
-      
-      <div>
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="white"
-        />
-        <Spotlight
-          className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="purple"
-        />
-        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
-      </div>
-
-      {/* Background grid */}
-      {/* <div
-        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
-        absolute top-0 left-0 flex items-center justify-center"
-      >
-        <div
-          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
-        bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
-        />
-      </div> */}
-
-      {/* Main content */}
-      <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-left justify-left">
-          {/* <TextGenerateEffect */}
-            {/* words="Hi, welcome to my portfolio!" */}
-            {/* className="text-left text-[40px] md:text-5xl lg:text-6xl"
-          /> */}
-          <h2 className="text-left text-[40px] md:text-5xl lg:text-6xl">Hi, welcome to my portfolio!</h2>
-
-          <TypeWriterEffect
-            prefix="I'm "
-            words={ words }
-            className="text-left sm:text-base md:text-xl lg:text-3xl xl:text-5xl"
-            cursorClassName="bg-blue-600"
+    <div className="bg-white-akari-white">
+      <Rain>
+        
+        <div>
+          <Spotlight
+            className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+            fill="white"
           />
-
-          <img src="/kitty.png" alt="Kitty" className="w-11" />
-
-          {/* <a href="#about">
-            <MagicButton
-              title="Show my work"
-              icon={<FaLocationArrow />}
-              position="right"
-            />
-          </a> */}
+          <Spotlight
+            className="h-[80vh] w-[50vw] top-10 left-full"
+            fill="purple"
+          />
+          <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
         </div>
-      </div>
+
+        {/* Background grid */}
+        {/* <div
+          className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+          absolute top-0 left-0 flex items-center justify-center"
+        >
+          <div
+            className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+          />
+        </div> */}
+
+        {/* Main content */}
+        <div className="flex justify-center relative my-20 z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-left justify-left">
+            {/* <TextGenerateEffect */}
+              {/* words="Hi, welcome to my portfolio!" */}
+              {/* className="text-left text-[40px] md:text-5xl lg:text-6xl"
+            /> */}
+            <h2 className="text-left text-[40px] md:text-5xl lg:text-6xl">Hi, welcome to my portfolio!</h2>
+
+            <TypeWriterEffect
+              prefix="I'm "
+              words={ words }
+              className="text-left sm:text-base md:text-xl lg:text-3xl xl:text-5xl"
+              cursorClassName="bg-blue-600"
+            />
+
+            <img src="/kitty.png" alt="Kitty" className="w-11" />
+
+            {/* <a href="#about">
+              <MagicButton
+                title="Show my work"
+                icon={<FaLocationArrow />}
+                position="right"
+              />
+            </a> */}
+          </div>
+        </div>
+      </ Rain>
     </div>
   );
 };
