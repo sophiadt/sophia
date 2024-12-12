ecfd9fe4fa49f48ddd15b7fab821b56cdc25aeb9
Sophia Don Tranho <sophiadnth@gmail.com>
2024-12-11 23:29:38 -0800

    adding recent projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
+import { navItems } from "@/data";
+
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { FaHome } from "react-icons/fa";
+import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
   return (
     <main className="relative bg-white-akari-white flex justify-center items-center flex-col overflow-hidden mx-auto">
       <div className="w-full">
-        <FloatingNav navItems={[
-          {name: "Home", link: "/", icon: <FaHome />}
-        ]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
+        <RecentProjects />
       </div>
     </main>
   );
