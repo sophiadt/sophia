437b0c6a7fab0803e3b9c8c3bd9f891ca1c32446
Sophia Don Tranho <sophiadnth@gmail.com>
2024-12-11 23:51:08 -0800

    pages and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { navItems } from "@/data";
 
-import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { FaHome } from "react-icons/fa";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
 import RecentProjects from "@/components/RecentProjects";
+import TechnicalProjects from "@/components/Technical";
+import GraphicalProjects from "@/components/Graphical";
+import Contact from "@/components/Contact";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 export default function Home() {
   return (
@@ -11,7 +15,18 @@ export default function Home() {
       <div className="w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
-        <RecentProjects />
+        <section id="about">
+          <About />
+        </section>
+        <section id="technical">
+          <TechnicalProjects />
+        </section>
+        <section id="graphical">
+          <GraphicalProjects />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
       </div>
     </main>
   );
