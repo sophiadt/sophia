5836ba685cdddf34fe4e8471d0dc0a2870425150
Sophia Don Tranho <sophiadnth@gmail.com>
2024-12-15 15:41:47 -0800

    newline

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -125,6 +125,7 @@ export const BentoGridItem = ({
                         titleClassName,
                         "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
                     )}
+                    style={{ whiteSpace: "pre-wrap" }}
                 >
                     {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
                     <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
--- data/index.ts
+++ data/index.ts
@@ -7,11 +7,21 @@ export const navItems = [
 ];
 
 export const gridItems = [
+    // {
+    //     id: 1,
+    //     title: "I prioritize client collaboration, fostering open communication ",
+    //     description: "",
+    //     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    //     imgClassName: "w-full h-full",
+    //     titleClassName: "justify-end",
+    //     img: "/b1.svg",
+    //     spareImg: "",
+    // },
     {
         id: 1,
-        title: "I prioritize client collaboration, fostering open communication ",
-        description: "",
-        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        title: "Software development, game development, UI/UX, and AI",
+        description: "Passionate in",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
         imgClassName: "w-full h-full",
         titleClassName: "justify-end",
         img: "/b1.svg",
@@ -19,10 +29,9 @@ export const gridItems = [
     },
     {
         id: 2,
-        title: `WiCS Events Coordinator
-                Secretary`,
-        description: "My Experience",
-        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        title: "Software Developer Intern @ Improving\nFrontend Developer @ Project Tech Careers\nEvents Coordinator - Women in Computing Science Club\nEvents Coordinator - Developer and Systems Club\nWebsite Developer Co-Lead - Developer and Systems Club\nSecretary - Women in Computing Science Club\nPhotographer - Volunteer",
+        description: "Experience",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
         imgClassName: "",
         titleClassName: "justify-start",
         img: "",
@@ -56,8 +65,8 @@ export const gridItems = [
         className: "md:col-span-3 md:row-span-2",
         imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
         titleClassName: "justify-center md:justify-start lg:justify-center",
-        img: "/b5.svg",
-        spareImg: "/grid.svg",
+        img: "/racoon.gif",
+        spareImg: "",
     },
     {
         id: 6,
