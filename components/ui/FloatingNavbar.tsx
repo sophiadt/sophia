5c0bb31f8f57a5b3b5446f0e96328715a338a7e6
sophiadt <sophiadnth@gmail.com>
2024-12-16 12:23:48 -0800

    mAN IDK NAVBAR MULTIPLY

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -58,17 +58,17 @@ export const FloatingNav = ({
                     duration: 0.2,
                 }}
                 className={cn(
-                    // change rounded-full to rounded-lg
-                    // remove dark:border-white/[0.2] dark:bg-black bg-white border-transparent
-                    // change  pr-2 pl-8 py-2 to px-10 py-5
                     "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4",
                     className
                 )}
                 style={{
-                    backdropFilter: "blur(16px) saturate(180%)",
-                    backgroundColor: "rgba(17, 25, 40, 0.75)",
-                    borderRadius: "12px",
-                    border: "1px solid rgba(255, 255, 255, 0.125)",
+                    background: "linear-gradient(rgb(0, 65, 206), rgb(121, 201, 255))",
+                    borderRadius: "16px",
+                    boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+                    backdropFilter: "blur(5px)",
+                    WebkitBackdropFilter: "blur(5.6px)",
+                    border: "1px solid rgba(157, 190, 207, 0.5)",
+                    mixBlendMode: "multiply",
                 }}
             >
                 {navItems.map((navItem: any, idx: number) => (
@@ -76,13 +76,13 @@ export const FloatingNav = ({
                         key={`link=${idx}`}
                         href={navItem.link}
                         className={cn(
-                            "relative items-center  flex space-x-1 text-neutral-100 hover:text-neutral-400"
+                            "relative items-center flex space-x-1 text-white hover:text-black-blue-300 mix-blend-normal"
                         )}
                     >
                         <span className="block sm:hidden">{navItem.icon}</span>
                         {/* add !cursor-pointer */}
                         {/* remove hidden sm:block for the mobile responsive */}
-                        <span className=" text-sm !cursor-pointer">{navItem.name}</span>
+                        <span className="!cursor-pointer">{navItem.name}</span>
                     </Link>
                 ))}
             </motion.div>
