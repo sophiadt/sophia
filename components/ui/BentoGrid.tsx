cb550984b82bc8860dec9ccd6ae680ec85c6ee2c
Sophia Don Tranho <sophiadnth@gmail.com>
2024-12-15 22:24:18 -0800

    tech stech stack

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -53,8 +53,8 @@ export const BentoGridItem = ({
     titleClassName?: string;
     spareImg?: string;
 }) => {
-    const leftLists = ["ReactJS", "Express", "Typescript"];
-    const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+    const leftLists = ["Python", "C++", "Java"];
+    const rightLists = ["JS", "NextJS", "SQL"];
 
     const [copied, setCopied] = useState(false);
 
@@ -149,20 +149,20 @@ export const BentoGridItem = ({
                                     <span
                                         key={i}
                                         className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    lg:opacity-100 rounded-lg text-center bg-blue-akari text-white"
                                     >
                                         {item}
                                     </span>
                                 ))}
-                                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-blue-akari"></span>
                             </div>
                             <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
-                                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-blue-akari"></span>
                                 {rightLists.map((item, i) => (
                                     <span
                                         key={i}
                                         className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    lg:opacity-100 rounded-lg text-center bg-blue-akari text-white"
                                     >
                                         {item}
                                     </span>
