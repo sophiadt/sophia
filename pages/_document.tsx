2f23b8ccebd035748502fa253e891b95a979b8ea
Sophia Don Tranho <66497192+sophiadt@users.noreply.github.com>
2024-12-16 19:43:02 -0800

    aaa undefinfed document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,20 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap"
-                        rel="stylesheet"
-                    />
-                </Head>
-                <body className="antialiased">
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+export default function Document() {
+    return (
+    <Html>
+        <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap"
+                rel="stylesheet"
+            />
+        </Head>
+        <body className="antialiased">
+            <Main />
+            <NextScript />
+        </body>
+    </Html>
+    )
 }
-
-export default MyDocument
